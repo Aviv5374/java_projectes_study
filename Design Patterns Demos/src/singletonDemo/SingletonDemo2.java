dc93729476da47086b5be02c406f09fd6d830f41
Aviv5374 <Aviv5374@gmail.com>
2017-04-20 11:05:23 +0300

    setup test7

---
+++ Design Patterns Demos/src/singletonDemo/SingletonDemo2.java
@@ -0,0 +1,12 @@
+package singletonDemo;
+
+public class SingletonDemo2 {
+
+	   public static void main(String[] args) {
+	      Singleton tmp = Singleton.getInstance( );
+	      tmp.demoMethod( );
+	      System.out.println(tmp);
+	      
+	      //659e0bfd
+	   }
+	}
