5fa97484986954ec9739ac5955eb45aa674088f9
Aviv5374 <Aviv5374@gmail.com>
2017-04-21 15:54:36 +0300

    setup working on code 3

--- Design Patterns HomeWork/singletonHomework‬/roomAndManager/Chair.java
+++ Design Patterns HomeWork/singletonHomework‬/roomAndManager/Chair.java
@@ -1,5 +1,13 @@
 package roomAndManager;
 
 class Chair {
-     private boolean taken;
+     private boolean taken = false;
+
+	public boolean isTaken() {
+		return taken;
+	}
+
+	public void setTaken(boolean taken) {
+		this.taken = taken;
+	}
 }
--- Design Patterns HomeWork/singletonHomework‬/student/Student.java
+++ Design Patterns HomeWork/singletonHomework‬/student/Student.java
@@ -1,5 +1,38 @@
 package student;
 
 public class Student {
+   private String sudentID;
+   
+   public Student(String sudentID, String studentName) {
+	super();
+	this.sudentID = sudentID;
+	this.studentName = studentName;
+}
+   
+public String getSudentID() {
+	return sudentID;
+}
 
+public void setSudentID(String sudentID) {
+	this.sudentID = sudentID;
+}
+
+public String getStudentName() {
+	return studentName;
+}
+
+public void setStudentName(String studentName) {
+	this.studentName = studentName;
+}
+
+public int getSitInRoomNumber() {
+	return sitInRoomNumber;
+}
+
+public void setSitInRoomNumber(int sitInRoomNumber) {
+	this.sitInRoomNumber = sitInRoomNumber;
+}
+
+private String studentName;
+   private int sitInRoomNumber;
 }
