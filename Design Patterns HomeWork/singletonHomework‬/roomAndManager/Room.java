b562c0ab37341d3dabc8423d2ca65a59e4f233a3
Aviv5374 <Aviv5374@gmail.com>
2017-04-24 16:08:44 +0300

    setup working on code 3

--- Design Patterns HomeWork/singletonHomework‬/roomAndManager/Room.java
+++ Design Patterns HomeWork/singletonHomework‬/roomAndManager/Room.java
@@ -1,5 +1,37 @@
 package roomAndManager;
 
 public class Room {
- 
+  private enum RoomState{Full,HaifFull,Free};
+  private RoomState roomState;
+  private final int roomNumber;
+  private int roomSize;
+  private Chair[] chairsInRoom;
+  
+
+public RoomState getRoomState() {
+	return roomState;
+}
+
+public void setRoomState(RoomState roomState) {
+	this.roomState = roomState;
+}
+
+public int getRoomSize() {
+	return roomSize;
+}
+//?
+private void setRoomSize(int roomSize) {
+	this.roomSize = roomSize;
+}
+public Chair[] getChairsInRoom() {
+	return chairsInRoom;
+}
+public void setChairsInRoom(Chair[] chairsInRoom) {
+	this.chairsInRoom = chairsInRoom;
+}
+public int getRoomNumber() {
+	return roomNumber;
+}
+  
+  
 }
