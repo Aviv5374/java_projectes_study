4b0017057aee6efb7abf986d6cb93167dc301d29
Aviv5374 <Aviv5374@gmail.com>
2017-05-04 09:49:39 +0300

    builder exercise 1 2

--- Design Patterns HomeWork/builderHomeWork1/builderAndTester/LeasingCar.java
+++ Design Patterns HomeWork/builderHomeWork1/builderAndTester/LeasingCar.java
@@ -5,16 +5,42 @@ import java.sql.Date;
 
 import javax.xml.crypto.Data;
 
+import builderExample.User.Builder;
+
 public class LeasingCar {
+	private static int currentId = 0;
 	
+	// requiered fields
 	private String licenseNumber;
 	private String companyName;
-	
+	// optional fields
 	private Date yearOfManufacture;
 	private Color color;
 	private static enum LevelOfEquipment {BASIC, MEDIUM, COMFORT} ;
 	private LevelOfEquipment levelOfEquipment;
 	private double price;
+	
+	private LeasingCar(Builder builder) {
+		super();
+		this.licenseNumber = builder.licenseNumber;
+		this.companyName = builder.companyName;
+		this.yearOfManufacture = builder.yearOfManufacture;
+		this.color = builder.color;
+		this.levelOfEquipment = builder.levelOfEquipment;
+		this.price = builder.price;
+	}
+	
+	public static class Builder{
+		// requiered fields
+		private String licenseNumber;
+		private String companyName;
+		// optional fields
+		private Date yearOfManufacture;
+		private Color color;
+		private static enum LevelOfEquipment {BASIC, MEDIUM, COMFORT} ;
+		private LevelOfEquipment levelOfEquipment;
+		private double price;
+	}
 	
 
 }
