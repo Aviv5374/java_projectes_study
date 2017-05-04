a7a16e4737ef967f9a2bc329588b01f9a7e76041
Aviv5374 <Aviv5374@gmail.com>
2017-05-04 09:45:24 +0300

    builder exercise 1 1

--- Design Patterns HomeWork/builderHomeWork1/builderAndTester/LeasingCar.java
+++ Design Patterns HomeWork/builderHomeWork1/builderAndTester/LeasingCar.java
@@ -1,5 +1,20 @@
 package builderAndTester;
 
+import java.awt.Color;
+import java.sql.Date;
+
+import javax.xml.crypto.Data;
+
 public class LeasingCar {
+	
+	private String licenseNumber;
+	private String companyName;
+	
+	private Date yearOfManufacture;
+	private Color color;
+	private static enum LevelOfEquipment {BASIC, MEDIUM, COMFORT} ;
+	private LevelOfEquipment levelOfEquipment;
+	private double price;
+	
 
 }
