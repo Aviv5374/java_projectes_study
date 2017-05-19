4002a1da7c441d3f47c10f542e892c3a274a3c64
Aviv5374 <Aviv5374@gmail.com>
2017-05-19 09:23:42 +0300

    Strategy 1

--- Design Patterns HomeWork/strategyHW/strategyHW‬/DemoApp.java
+++ Design Patterns HomeWork/strategyHW/strategyHW‬/DemoApp.java
@@ -2,6 +2,14 @@ package strategyHW?;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.Box;
+import java.awt.TextField;
+import java.awt.Panel;
+import java.awt.Label;
+import java.awt.Font;
+import java.awt.Button;
 
 public class DemoApp {
 
@@ -37,6 +45,31 @@ public class DemoApp {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		TextField numberTextField = new TextField();
+		numberTextField.setFont(new Font("Dialog", Font.PLAIN, 13));
+		numberTextField.setBounds(152, 57, 243, 19);
+		frame.getContentPane().add(numberTextField);
+		
+		Label NumberLabel = new Label("Enter Number");
+		NumberLabel.setFont(new Font("Dialog", Font.PLAIN, 13));
+		NumberLabel.setBounds(10, 57, 107, 19);
+		frame.getContentPane().add(NumberLabel);
+		
+		Label emailLabel = new Label("Enter Email");
+		emailLabel.setFont(new Font("Dialog", Font.PLAIN, 13));
+		emailLabel.setBounds(10, 117, 107, 19);
+		frame.getContentPane().add(emailLabel);
+		
+		TextField emailTextField = new TextField();
+		emailTextField.setFont(new Font("Dialog", Font.PLAIN, 13));
+		emailTextField.setBounds(152, 117, 243, 19);
+		frame.getContentPane().add(emailTextField);
+		
+		Button okButton = new Button("OK");
+		okButton.setFont(new Font("Dialog", Font.PLAIN, 13));
+		okButton.setBounds(152, 200, 86, 30);
+		frame.getContentPane().add(okButton);
 	}
-
 }
